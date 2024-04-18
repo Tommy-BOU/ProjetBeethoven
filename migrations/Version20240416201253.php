39a4c33cda2297a05e7bd749e84533b0fed4344c
vmorin <virginiemorin@yahoo.com>
2024-04-18 09:35:12 +0200

    update jeudi

--- migrations/Version20240416201253.php
+++ migrations/Version20240416201253.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240415154841 extends AbstractMigration
+final class Version20240416201253 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,18 +20,16 @@ final class Version20240415154841 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE book (id INT AUTO_INCREMENT NOT NULL, state_id INT NOT NULL, author VARCHAR(255) NOT NULL, publication_year INT NOT NULL, title VARCHAR(255) NOT NULL, summary LONGTEXT NOT NULL, image VARCHAR(255) NOT NULL, available TINYINT(1) NOT NULL, global_rating DOUBLE PRECISION NOT NULL, INDEX IDX_CBE5A3315D83CC1 (state_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE state (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE book ADD state_id INT NOT NULL');
         $this->addSql('ALTER TABLE book ADD CONSTRAINT FK_CBE5A3315D83CC1 FOREIGN KEY (state_id) REFERENCES state (id)');
-        $this->addSql('CREATE INDEX IDX_CBE5A3315D83CC1 ON book (state_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE book DROP FOREIGN KEY FK_CBE5A3315D83CC1');
+        $this->addSql('DROP TABLE book');
         $this->addSql('DROP TABLE state');
-        $this->addSql('DROP INDEX IDX_CBE5A3315D83CC1 ON book');
-        $this->addSql('ALTER TABLE book DROP state_id');
     }
 }
